974e92f6fff03c51f582cbea7c8e7f515252c11c
Aravind Elangovan <filch20@gmail.com>
2016-10-08 12:40:33 -0400

    Initial object generation code.

--- Assets/TreeGenerator.cs
+++ Assets/TreeGenerator.cs
@@ -1,11 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ForestGenerator : MonoBehaviour {
+public class TreeGenerator : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		Instantiate(tree, gameObject.transform);
+	
 	}
 	
 	// Update is called once per frame
