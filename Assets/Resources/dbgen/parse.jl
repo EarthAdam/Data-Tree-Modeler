41d68976c29bad50c6c0630eb6706e9e00ace048
Tim Besard <tim.besard@gmail.com>
2016-10-08 12:47:33 -0400

    Add progress bar.

--- Assets/Resources/dbgen/parse.jl
+++ Assets/Resources/dbgen/parse.jl
@@ -23,6 +23,8 @@ end
 # Tree construction
 #
 
+using ProgressMeter
+
 abstract Node
 
 abstract LeafNode <: Node
@@ -114,9 +116,11 @@ end
 function Tree(sources)
     root = DirectoryNode()
 
+    p = Progress(length(sources), 1)
     for path in sources
         rel = relpath(path, DIR)
         add_file!(root, rel)
+        next!(p)
     end
 
     return root
