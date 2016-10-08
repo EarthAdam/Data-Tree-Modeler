12f11ad59452efabf9b4f2647fec5beb105508e5
Aravind Elangovan <filch20@gmail.com>
2016-10-08 14:19:47 -0400

    Generate object tree from json file.

--- Assets/ForestGenerator.cs
+++ Assets/ForestGenerator.cs
@@ -1,17 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
+using SimpleJSON;
 
 public class ForestGenerator : MonoBehaviour {
 
+	//perhaps pull out into a 'prefabs cache' type class
+	public static Tree treePrefab = Resources.Load<Tree>("Tree.prefab");
+
 	// Use this for initialization
 	void Start () {
-		String filePath = "dbgen/sample.json";
-		TextAsset targetFile = Resources.Load<TextAsset>(filePath);
-		GameObject tree = Instantiate(tree, gameObject.transform) as GameObject;
+		Tree root = Instantiate(treePrefab, gameObject.transform) as Tree;
+		
+		string filePath = "dbgen/sample.json";
+		TextAsset jsonFile = Resources.Load<TextAsset>(filePath);
+		var jsonData = JSON.Parse(jsonFile.text);
+
+		root.GetComponent<TreeGenerator>().generate(jsonData);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+
 }
--- Assets/TreeGenerator.cs
+++ Assets/TreeGenerator.cs
@@ -3,10 +3,25 @@ using System.Collections;
 
 public class TreeGenerator : MonoBehaviour {
 
+	JSONClass files;
+	JSONClass directories;
+
 	// Use this for initialization
 	void Start () {
 	
 	}
+
+	void Generate(string name, JSONClass jsonData) {
+		files = jsonData["files"];
+		directories = jsonData["directories"];
+
+		foreach(KeyValuePair kv in directories) {
+			Tree branch = Instantiate(ForestGenerator.treePrefab, gameObject.transform) as Tree;
+			branch.GetComponent<TreeGenerator>().generate(kv.Key, kv.Value); //send the folder name and subdirectories
+		}
+	}
+
+	static void 
 	
 	// Update is called once per frame
 	void Update () {
